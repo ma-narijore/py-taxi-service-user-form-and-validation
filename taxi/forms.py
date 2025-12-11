e1b1d4b04a164fec0f2d6416493f3418cf575b9c
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-12-11 16:46:43 +0000

    fix_3

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,48 +1,35 @@
 from django.contrib.auth import get_user_model
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Driver, Car
+from .models import Car
 import re
 
 
-class DriverLicenseUpdateForm(forms.ModelForm):
+class LicenseNumberValidationMixin:
+    def clean_license_number(self):
+        value = self.cleaned_data.get("license_number", "")
+
+        pattern = r"^[A-Z]{3}\d{5}$"
+
+        if not re.match(pattern, value):
+            raise forms.ValidationError(
+                "License number must have 3 uppercase letters followed by 5 digits. "
+                "Example: ABC12345"
+            )
+
+        return value
+
+
+class DriverLicenseUpdateForm(LicenseNumberValidationMixin, forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ("license_number",)
 
-    def clean_license_number(self):
-        value = self.cleaned_data["license_number"]
 
-        pattern = r"^[A-Z]{3}\d{5}$"
-
-        if not re.match(pattern, value):
-            raise forms.ValidationError(
-                "License number must have"
-                " 3 uppercase letters followed by 5 digits."
-                " Example: ABC12345"
-            )
-
-        return value
-
-
-class DriverAddForm(UserCreationForm, DriverLicenseUpdateForm):
+class DriverAddForm(LicenseNumberValidationMixin, UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + ("license_number",)
-
-    def clean_license_number(self):
-        value = self.cleaned_data["license_number"]
-
-        pattern = r"^[A-Z]{3}\d{5}$"
-
-        if not re.match(pattern, value):
-            raise forms.ValidationError(
-                "License number must have"
-                " 3 uppercase letters followed by 5 digits."
-                " Example: ABC12345"
-            )
-
-        return value
 
 
 class CarForm(forms.ModelForm):
